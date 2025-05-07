e57233939c1a29b4f1fbf1073b9eb3a7677cbbc4
Navid Zare <naavidzaare@gmail.com>
2025-05-08 02:14:17 +0330

    WIP

---
+++ src/JMLS.Domain/Points/Rules/CustomerIdMustBeMoreThanZeroRule.cs
@@ -0,0 +1,11 @@
+﻿namespace JMLS.Domain.Points.Rules;
+
+public class CustomerIdMustBeMoreThanZeroRule(int customerId) : IBusinessRule
+{
+    public bool IsBroken()
+    {
+        return customerId <= 0;
+    }
+
+    public string Message => "Customer Id must be more than zero.";
+}
